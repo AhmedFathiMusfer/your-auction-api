aa60d83c31f9bb140c6005b1daf36411f1e6eb33
AhmedFathiMusfer <ahmdfthymsfra@gmail.com>
2026-06-19 02:45:01 +0300

    Update NuGet packages and project references
    - Updated package versions in project.nuget.cache and your-auction-api.csproj.nuget.dgspec.json to the latest compatible versions.
    - Added OpenAI package reference (version 2.9.1) to your-auction-api.csproj.
    - Adjusted paths in project files to use lowercase for consistency.
    - Updated targets in your-auction-api.csproj.nuget.g.targets to reflect new package versions.
    - Added warning log for package vulnerability data retrieval issue.

--- obj/Debug/net8.0/your-auction-api.AssemblyInfo.cs
+++ obj/Debug/net8.0/your-auction-api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("your-auction-api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+02a0090ab2da1c6a147f942b11e59f3686dfe409")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de2b841ecf847dfb96b9ad52ddddb4f6e3ddcba4")]
 [assembly: System.Reflection.AssemblyProductAttribute("your-auction-api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("your-auction-api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
