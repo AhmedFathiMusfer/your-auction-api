833e78cfc112783a9db410fd58ac58962f664799
AhmedFathiMusfer <ahmdfthymsfra@gmail.com>
2026-06-24 00:49:30 +0300

    Update RegisterationRequestDTO to make ProfilePictureUrl nullable and remove Role property

--- Models/Dto/RegisterationRequestDTO.cs
+++ Models/Dto/RegisterationRequestDTO.cs
@@ -8,7 +8,6 @@
         public string Password { get; set; }
 
         public string PhoneNumber { get; set; }
-        public string ProfilePictureUrl { get; set; }
-        public string Role { get; set; }
+        public string? ProfilePictureUrl { get; set; }
     }
 }
